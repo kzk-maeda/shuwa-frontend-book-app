825de0124e1a9ccc1d50b7d349e54a86adaf87bf
kzk-maeda <kzk.maeda0711@gmail.com>
2020-12-31 00:44:10 +0900

    replaced to react

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,36 @@ module.exports = {
     mode: process.env.NODE_ENV || 'development',
     entry: './src/app.tsx',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist'
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist'
     },
     resolve: {
-        extensions: ['.js', '.ts', '.tsx']
-    },
-    devServer: {
-        contentBase: __dirname,
-        port: 5000
+      extensions: ['.js', '.ts', '.tsx']
     },
     module: {
-        rules: [
-            {
-                test: /\.(j|t)sx?$/,
-                exclude: /node_modules/,
-                use: {
-                        loader: 'babel-loader',
-                        options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-typescript',
-                            '@babel/preset-react'
-                        ]
-                    }
-                }
+      rules: [
+        {
+          test: /\.(j|t)sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ],
+              plugins: [
+                '@babel/plugin-transform-runtime'
+              ]
             }
-        ]
+          }
+        }
+      ]
     },
-};
+    devServer: {
+      contentBase: __dirname,
+      port: 5000
+    },
+  };
